d884d3588c38a0be17b10ea200104441ccf22610
Michael Hughes <TaurusSHO89@gmail.com>
2019-10-01 20:32:34 -0600

    Updated Breaking News service time and comment.

--- services/wow/breakingNews.js
+++ services/wow/breakingNews.js
@@ -3,8 +3,8 @@ const CronJob = require('cron').CronJob;
 const ServicesLogging = require('../../db/dbLogging');
 
 module.exports = {
-    // At 07 minutes past the hour, collect the "Breaking News" Alert from World of Warcraft.
-    get: (db) => new CronJob('00 048 0-23 * * 0-6', () => {
+    // At 48 minutes past the hour, collect the "Breaking News" Alert from World of Warcraft.
+    get: (db) => new CronJob('00 48 0-23 * * 0-6', () => {
         axios.get('http://launcher.worldofwarcraft.com/alert').then(res => {
             //Create object for DB Insert, removing Blizzard's HTML.
             let obj = {
