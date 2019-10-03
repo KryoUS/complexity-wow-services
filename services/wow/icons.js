3335ecbbce472fa48a13254c34c3eeb2c1e2e2a6
Michael Hughes <TaurusSHO89@gmail.com>
2019-10-02 21:53:29 -0600

    Fixed invalid API Key from environment and removed collection of non-existent item icons as null.

--- services/wow/icons.js
+++ services/wow/icons.js
@@ -3,24 +3,22 @@ const CronJob = require('cron').CronJob;
 const ServicesLogging = require('../../db/dbLogging');
 
 module.exports = {
-    // Every other hour, at 33 minutes past the hour, collect all Item IDs from the icons table. Grabs all icons that we don't already have from Blizzard.
+    // Every other hour, at 33 minutes past the hour, collect all Item IDs from the icons table. Then grabs all icons that we don't already have from Blizzard.
     getItemIcons: (db) => new CronJob('00 33 */2 * * *', () => {
         ServicesLogging(db, 'iconItems', `Item Icon collection started.`);
-        let addedIcons = [];
 
         db.query('select id from icons').then(response => {
-            
             let count = 1;
 
             for (i = 1; i < 200000; i++) {
 
                 if (response.findIndex(x => x.id === i) === -1) {
             
-                    const startItemIconCollection = (count, index) => {
+                    const startItemIconCollection = (count, index, db) => {
                 
                         setTimeout(() => {
                             
-                            axios.get(`https://us.api.blizzard.com/data/wow/media/item/${index}?namespace=static-us&locale=en_US&access_token=${process.env.BLIZZ_API_TOKEN}`).then(res => {
+                            axios.get(`https://us.api.blizzard.com/data/wow/media/item/${index}?namespace=static-us&locale=en_US&access_token=${process.env.BLIZZ_TOKEN}`).then(res => {
                                 
                                 let obj = {
                                     id: index,
@@ -28,45 +26,24 @@ module.exports = {
                                     iconurl: res.data.assets[0].value
                                 }
 
-                                req.app.get('db').icons.insert(obj).then(dbRes => {
-                                    addedIcons.push(obj.index);
-                                    if (obj.index === 200000) {
-                                        ServicesLogging(db, 'iconItems', `Icon Insertion complete.`, {icons: addedIcons});
-                                    }
+                                db.icons.insert(obj).then(dbRes => {
                                 }).catch(dbErr => {
                                     //Log to database an error in inserting data.
                                     ServicesLogging(db, 'iconItems', `DB insert error.`, dbErr);
                                 });
 
                             }).catch(wowErr => {
-
-                                if (wowErr.response.status === 404) {
-                                    let obj = {
-                                        id: index,
-                                        epoch_datetime: new Date().getTime(), 
-                                        iconurl: null
-                                    }
-
-                                    req.app.get('db').icons.insert(obj).then(dbRes => {
-                                        addedIcons.push(obj.index);
-                                        if (obj.index === 200000) {
-                                            ServicesLogging(db, 'iconItems', `Icon Insertion complete.`, {icons: addedIcons});
-                                        }
-                                    }).catch(dbErr => {
-                                        //Log to database an error in inserting data.
-                                        ServicesLogging(db, 'iconItems', `DB insert error.`, dbErr);
-                                    });
-                                } else {
+                                if (wowErr.response.status !== 404) {
                                     //Log to database an error in collecting data.
-                                    ServicesLogging(db, 'iconItems', `WoW API Error.`, wowErr);
+                                    ServicesLogging(db, 'iconItems', `WoW API Error.`, wowErr.response);
                                 }
 
                             })
 
-                        }, count * 50);
+                        }, count * 30);
                     }
 
-                    startItemIconCollection(count, index);
+                    startItemIconCollection(count, i, db);
                     count++;
                 }
 
