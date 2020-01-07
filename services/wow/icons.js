7ca64fdaa5a0ce44ca263ac239d5f54a8a7ef84e
Michael Hughes <TaurusSHO89@gmail.com>
2020-01-07 15:54:06 -0700

    Icon collection functions now work properly with correct timeouts and starting points.

--- services/wow/icons.js
+++ services/wow/icons.js
@@ -9,13 +9,13 @@ module.exports = {
 
         db.query('select id from icons order by id desc limit 1').then(response => {
 
-            let maxID = response[0];
+            let maxID = response[0].id;
             let count = 0;
             let maxCount = maxID + 10000;
 
             for (i = maxID; i < maxCount; i++) {
             
-                const startItemIconCollection = (iconID, db, maxCount) => {
+                const startItemIconCollection = (iconID, db, maxCount, count, startingID) => {
             
                     setTimeout(() => {
                         
@@ -28,26 +28,35 @@ module.exports = {
                             }
 
                             db.icons.insert(obj).then(dbRes => {
-                                if (iconID === maxCount) {
-                                    ServicesLogging(db, 'iconItems', `Item Icon collection finished. (${iconID} to ${maxCount})`);
-                                }
+
                             }).catch(dbErr => {
                                 //Log to database an error in inserting data.
                                 ServicesLogging(db, 'iconItems', `DB insert error.`, dbErr);
                             });
 
                         }).catch(wowErr => {
-                            if (wowErr.response.status !== 404) {
+                            
+                            if (wowErr.errno == 'ETIMEDOUT') {
+                                //Log to database an error in collecting data.
+                                ServicesLogging(db, 'iconSpells', `WoW API Timeout.`, wowErr);
+                            }
+                            
+                            if (wowErr.response && wowErr.response.status !== 404) {
                                 //Log to database an error in collecting data.
                                 ServicesLogging(db, 'iconItems', `WoW API Error.`, wowErr.response);
                             }
 
-                        })
+                        });
 
-                    }, count * 250); //This runs at 4 per second which ends up being 14,400 Blizzard API Calls per hour. (Blizzard cap is 36,000 per hour.)
+                        if (iconID === maxCount) {
+                            ServicesLogging(db, 'iconItems', `Item Icon collection finished. (${startingID} to ${maxCount})`);
+                        }
+
+                    }, 250 * count); //This runs at 4 per second which ends up being 14,400 Blizzard API Calls per hour. (Blizzard cap is 36,000 per hour.)
                 }
 
-                startItemIconCollection(i, db, maxCount);
+                startItemIconCollection(i, db, maxCount, count, maxID);
+                count++;
 
             }
         }).catch(error => {
@@ -61,45 +70,54 @@ module.exports = {
         
         ServicesLogging(db, 'iconSpells', `Spell Icon collection started.`);
 
-        db.query('select * from spellicons order by id desc limit 1').then(response => {
+        db.query('select id from spellicons order by id desc limit 1').then(response => {
 
-            let maxID = response[0];
+            let maxID = response[0].id;
             let count = 0;
             let maxCount = maxID + 10000;
 
-            for (i = maxID; i < maxCount; i++) {
+            for (let i = maxID; i < maxCount; i++) {
             
-                const startItemIconCollection = (spelliconID, db, maxCount) => {
+                const startItemIconCollection = (spelliconID, db, maxCount, count, startingID) => {
             
                     setTimeout(() => {
-                        
+
                         //NOTE: This is the old Blizzard API since they do not have a Media endpoint for Spell Icons yet. 2019-10-26
                         axios.get(`https://us.api.blizzard.com/wow/spell/${spelliconID}?locale=en_US&access_token=${process.env.BLIZZ_TOKEN}`).then(res => {
-                            
+
                             res.data.iconurl = `https://render-us.worldofwarcraft.com/icons/56/${res.data.icon}.jpg`;
                             res.data.unix_datetime = new Date().getTime();
                             res.data.casttime = res.data.castTime;
 
                             db.spellicons.insert(res.data).then(dbRes => {
-                                if (spelliconID === maxCount) {
-                                    ServicesLogging(db, 'iconSpells', `Spell Icon collection finished. (${spelliconID} to ${maxCount})`);
-                                }
+                                
                             }).catch(dbErr => {
                                 //Log to database an error in inserting data.
                                 ServicesLogging(db, 'iconSpells', `DB insert error.`, dbErr);
                             });
 
                         }).catch(wowErr => {
-                            if (wowErr.response.data.status !== 'nok') {
+
+                            if (wowErr.errno == 'ETIMEDOUT') {
+                                //Log to database an error in collecting data.
+                                ServicesLogging(db, 'iconSpells', `WoW API Timeout.`, wowErr);
+                            }
+
+                            if (wowErr.response && wowErr.response.status !== 404) {
                                 //Log to database an error in collecting data.
                                 ServicesLogging(db, 'iconSpells', `WoW API Error.`, wowErr.response);
                             }
-                        })
+                        });
 
-                    }, count * 250); //This runs at 4 per second which ends up being 14,400 Blizzard API Calls per hour. (Blizzard cap is 36,000 per hour.)
+                        if (spelliconID === maxCount) {
+                            ServicesLogging(db, 'iconSpells', `Spell Icon collection finished. (${startingID} to ${maxCount})`);
+                        }
+
+                    }, 250 * count); //This runs at 4 per second which ends up being 14,400 Blizzard API Calls per hour. (Blizzard cap is 36,000 per hour.)
                 }
 
-                startItemIconCollection(i, db, maxCount);
+                startItemIconCollection(i, db, maxCount, count, maxID);
+                count++;
             }
         }).catch(error => {
             ServicesLogging(db, 'iconSpells', `DB fetch error.`, error);
