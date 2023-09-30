4c376b70e4f5328c56b8bb3149ec800090a8ae61
KryoUS <taurussho89@gmail.com>
2023-09-29 20:23:23 -0600

    Updated Services Logging to grab DB dynamically. Added Services Log Cleanup method.

--- db/dbLogging.js
+++ db/dbLogging.js
@@ -1,15 +1,28 @@
-//Discord Bot Logging
+const getDb = require('./db');
 const functions = require('../tools/functions');
 
-module.exports = (db, category, message, errorJSON) => {
-    db.serviceslog.insert({ 
-        epoch_datetime: new Date().getTime(),
-        category: category,  
-        message: message,
-        info: errorJSON ? JSON.stringify(errorJSON, functions.getCircularReplacer()) : '{}'
-    }).then(result => {
-        //Do nothing with results
-    }).catch(error => {
-        console.log(`${new Date()} Massive.js CharacterCronLogging Insert Error = `, error);
-    })
-};
+module.exports = {
+    servicesLogging: async (category, message, errorJSON) => {
+
+        let db = await getDb();
+
+        db.serviceslog.insert({
+            epoch_datetime: new Date().getTime(),
+            category: category,
+            message: message,
+            info: errorJSON ? JSON.stringify(errorJSON, functions.getCircularReplacer()) : '{}'
+        }).then(result => {
+            //Do nothing with results
+        }).catch(error => {
+            console.log(`${new Date()} Massive.js CharacterCronLogging Insert Error = `, error);
+        })
+    },
+
+    ServicesLoggingCleanup: async () => {
+        let db = await getDb();
+
+        db.query("DELETE FROM serviceslog WHERE created_at < NOW() - INTERVAL '15 days'").catch(dbError => {
+            console.log(`${new Date()} ===Massive.js DB Bnet Log Cleanup Error===`, dbError);
+        });
+    }
+}
